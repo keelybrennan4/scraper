5958b2dcf04b63bc8d273f8895286b6f07902e00
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-06-03 22:27:37 -0500

    added mongoose connection and added folder organization

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
-// just your basic server 
+// basic server 
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
 //require the router 
-var htmlRoutes = require("./routes/htmlRoutes")
-var apiRoutes = require("./routes/apiRoutes")
+var htmlRoutes = require("./config/routes/htmlRoutes")
+var apiRoutes = require("./config/routes/apiRoutes")
 
-const port = process.env.PORT || 3000;
+//require mongoose
+var mongoose = require("mongoose");
 
-//to serve files 
+//set port 
+var port = process.env.PORT || 3000;
+
+// serve static html css and js
 app.use(express.static("public"));
 
-// to use the htmlRoutes we're requiring above 
+//to use the htmlRoutes we're requiring above 
 app.use(htmlRoutes);
 
 //route = /api/posts because it's concatinating the route on apiRoutes 
 app.use("/api", apiRoutes);
 
-//add hbs below routes 
+//hbs routes 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+
+//if deployed, connect to deployed databse. Otherwise, connect to localhost tcArticles db
+var mongoConnect = process.env.MONGODB_URI ||"mongodb://localhost/tcArticles"
+
+// connect to mongo db
+mongoose.connect(mongoConnect, function(error){
+    if (error){
+        console.log(error);
+    } else{
+        console.log("mongoose is connected");
+    }
+});
 
 app.listen(port, function(){
     console.log("app is running");
